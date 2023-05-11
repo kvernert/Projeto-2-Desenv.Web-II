27ee055a490ad46d33563a0677e05a647e273371
kvernet <kverner47@gmail.com>
2023-05-10 22:01:34 -0300

    commit dia 10/05

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+const items = [
+    {
+        id: 1,
+        nome: 'Camiseta',
+        preco: 49.90
+    },
+    {
+        id: 2,
+        nome: 'Calça',
+        preco: 99.90
+    },
+    {
+        id: 3,
+        nome: 'Meia',
+        preco: 9.90
+    },
+    {
+        id: 4,
+        nome: 'Sapato',
+        preco: 199.90
+    },
+    {
+        id: 5,
+        nome: 'Boné',
+        preco: 29.90
+    },
+    {
+        id: 6,
+        nome: 'Óculos',
+        preco: 99.90
+    },
+    {
+        id: 7,
+        nome: 'Relógio',
+        preco: 299.90
+    },
+    {
+        id: 8,
+        nome: 'Bermuda',
+        preco: 79.90
+    },
+    {
+        id: 9,
+        nome: 'Cueca',
+        preco: 19.90
+    },
+    {
+        id: 10,
+        nome: 'Meia',
+        preco: 9.90
+    }
+]
+</script>
+<template>
+  <div>
+    <ul>
+    <li v-for="item in items" :key="item.id">
+      <p>Nome: {{ item.nome }}</p>
+    </li>
+  </ul>
+  </div>
+</template>
