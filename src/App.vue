d962bfe2d90fbeeff2090fb4cbc4889960bd7a5c
kvernet <kverner47@gmail.com>
2023-05-11 16:46:01 -0300

    commit do dia 11/05

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,127 @@
 <script setup>
-const items = [
-    {
-        id: 1,
-        nome: 'Camiseta',
-        preco: 49.90
-    },
-    {
-        id: 2,
-        nome: 'Calça',
-        preco: 99.90
-    },
-    {
-        id: 3,
-        nome: 'Meia',
-        preco: 9.90
-    },
-    {
-        id: 4,
-        nome: 'Sapato',
-        preco: 199.90
-    },
-    {
-        id: 5,
-        nome: 'Boné',
-        preco: 29.90
-    },
-    {
-        id: 6,
-        nome: 'Óculos',
-        preco: 99.90
-    },
-    {
-        id: 7,
-        nome: 'Relógio',
-        preco: 299.90
-    },
-    {
-        id: 8,
-        nome: 'Bermuda',
-        preco: 79.90
-    },
-    {
-        id: 9,
-        nome: 'Cueca',
-        preco: 19.90
-    },
-    {
-        id: 10,
-        nome: 'Meia',
-        preco: 9.90
-    }
-]
+import { ref } from 'vue';
+
+let valortotal = ref(0)
+
+const produtos = ref([
+  {
+    id: 1,
+    nome: 'Camiseta',
+    preco: 49.90,
+    quantidade: 1
+  },
+  {
+    id: 2,
+    nome: 'Calça',
+    preco: 99.90,
+    quantidade: 1
+  },
+  {
+    id: 3,
+    nome: 'Meia',
+    preco: 9.90,
+    quantidade: 1
+  },
+  {
+    id: 4,
+    nome: 'Sapato',
+    preco: 199.90,
+    quantidade: 1
+  },
+  {
+    id: 5,
+    nome: 'Boné',
+    preco: 29.90,
+    quantidade: 1
+  },
+  {
+    id: 6,
+    nome: 'Óculos',
+    preco: 99.90,
+    quantidade: 1
+  },
+  {
+    id: 7,
+    nome: 'Relógio',
+    preco: 299.90,
+    quantidade: 1
+  },
+  {
+    id: 8,
+    nome: 'Bermuda',
+    preco: 79.90,
+    quantidade: 1
+  },
+  {
+    id: 9,
+    nome: 'Cueca',
+    preco: 19.90,
+    quantidade: 1
+  },
+  {
+    id: 10,
+    nome: 'Meia',
+    preco: 9.90,
+    quantidade: 1
+  }
+])
+let carrinho = ref([])
+function addcarrinho(item) {
+  carrinho.value.push({
+    codigo: item.id,
+    nome: item.nome,
+    preco: item.preco,
+    quantidade: item.quantidade,
+    totalitem: item.preco * item.quantidade
+  })
+  totalvalor()
+  item.quantidade = 1
+}
+function totalvalor(){
+  for (let contador = 0; contador < carrinho.value.length; contador++) {
+    valortotal.value = valortotal.value + carrinho.value[contador].totalitem  
+  }
+}
+function limpacarrinho() {
+  carrinho.value = []
+}
+function remover(index) {
+  carrinho.value.splice(index, 1)
+}
+const avf = (value) => "R$ " + value.toFixed(2).replace('.', ',')
 </script>
+
 <template>
-  <div>
+    <div class="nav">
+        <p></p>
+
+    </div>
+  <div class="carrinho">
+    <h1>carrinho {{ avf(valortotal) }}</h1> 
     <ul>
-    <li v-for="item in items" :key="item.id">
-      <p>Nome: {{ item.nome }}</p>
+      <li v-for="(item, index) in carrinho" :key="index">{{ item.nome }} {{avf(item.preco) }} {{
+        item.quantidade
+      }} valor total {{ (item.totalitem) }}
+        <button @click="remover(index)">remover item</button>
+      </li>
+    </ul>
+    <button @click="limpacarrinho()">limpar carrinho</button>
+  </div>
+  <ul>
+    <li v-for="(item, index) in produtos" :key="index">Item: {{ item.nome }} Valor: {{ avf(item.preco) }}
+      Quantidade: {{ item.quantidade }}
+      <button @click="addcarrinho(item)">adicionar</button>
+      <button @click="item.quantidade++">+</button>
+      <button @click="item.quantidade--" v-if="item.quantidade > 1">-</button>
     </li>
   </ul>
-  </div>
 </template>
+
+
+<style scoped>
+.nav{
+    background-color:black ;
+}
+</style>
+
+
